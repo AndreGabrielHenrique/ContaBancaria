fafa87b21a0ace86a9d2ad50c7527668569324d8
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-04-01 23:50:25 -0300

    Implementing comments at each step of the source code and adding functions with validation conditions

--- Cliente.cs
+++ Cliente.cs
@@ -1,31 +1,36 @@
 public class Cliente
 {
-    public string separador=new string('-',50);
+    //Variáveis
+    public string separador=new string('-',70);
     public string Nome{get;set;}
-    public int NumeroDaConta{get;set;}
+    public string NumeroDaConta{get;set;}
     public double Saldo{get;private set;}
     public double Deposito{get;set;}
-    public double Saque{get;set;}    
-    public Cliente(string nome,int numerodaconta)
+    public double Saque{get;set;}   
+    //Construtores 
+    public Cliente(string nome,string numerodaconta)
     {
         Nome=nome;
         NumeroDaConta=numerodaconta;        
     }
-    public Cliente(string nome,int numerodaconta,double saldo):this(nome,numerodaconta)
+    public Cliente(string nome,string numerodaconta,double saldo):this(nome,numerodaconta)
     {
         Saldo=saldo;        
     }
-    
-    public void RealizarDeposito(double Deposito)
+    //Funções
+    //Algoritmo para realizar depósito de dinheiro na conta, no final mostrando os dados no extrato
+    public void RealizarDeposito(double deposito)
     {
-        Saldo+=Deposito;
+        Saldo+=deposito;
         Extrato();
     }
+    //Algoritmo para realizar saque de dinheiro na conta, no final mostrando os dados no extrato
     public void RealizarSaque(double Saque)
     {
         Saldo-=Saque-5;
         Extrato();
     }
+    //Extrato bancário, com atribuição privada para evitar manuseio dos dados
     private void Extrato()
     {
         System.Console.WriteLine($"{separador}\nNome:                   {Nome}\nNúmero da conta:        {NumeroDaConta}\nSaldo:                  {Saldo:c}\n{separador}");
--- Program.cs
+++ Program.cs
@@ -8,37 +8,129 @@ O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Pa
 Nota: a conta pode ficar com saldo negativo se o saldo não for suficiente para realizar o saque e/ou pagar a taxa.
 Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não informado o valor de depósito inicial.
 Em seguida, realizar um depósito e depois um saque, sempre mostrando os dados da conta após cada operação.*/
-string separador=new string('-',50);
+//Variáveis
+string separador=new string('-',70);
+string nome,fazerdepositoinicial,numerodaconta,Deposito,Saque;
+//Lógica aplicada
 System.Console.WriteLine(separador);
-System.Console.WriteLine("Digite o nome");
-string nome=Console.ReadLine();
-System.Console.WriteLine("Digite o número da conta");
-int numerodaconta=int.Parse(Console.ReadLine());
-System.Console.WriteLine("Deseja realizar um depósito inicial?");
-string fazerdepositoinicial=Console.ReadLine();
-Cliente c1;
-if(fazerdepositoinicial=="Sim"||fazerdepositoinicial=="SIM"||fazerdepositoinicial=="sim")
+InserirNome();
+Cliente c1;  
+System.Console.WriteLine(separador);
+Console.ReadKey();
+Console.Clear();
+InserirDeposito();
+Console.ReadKey();
+Console.Clear();
+InserirSaque();
+Console.ReadKey();
+Console.Clear();
+//Funções
+//Inserir nome do cliente, com estrutura de condição para validar o input
+void InserirNome()
 {
+    System.Console.WriteLine("Digite o nome");
+    nome=Console.ReadLine();
+    if(string.IsNullOrWhiteSpace(nome)||nome.Length<5)
+    {
+        System.Console.WriteLine($"Nome do cliente em branco ou com menos de cinco caracteres, favor reinserir.\n{separador}");
+        InserirNome();
+    }
+    else
+    {
+        System.Console.WriteLine(separador);
+        InserirNumeroDaConta();    
+    }
+}
+//Inserir número da conta, com estrutura de condição para validar o input
+void InserirNumeroDaConta()
+{
+    System.Console.WriteLine("Digite o número da conta");
+    numerodaconta=Console.ReadLine();
+    bool validar=numerodaconta.Length==7&&numerodaconta.All(char.IsDigit);
+    if(validar==false)
+    {
+        System.Console.WriteLine($"Número com menos ou mais de 7 digitos, em branco ou com caracteres, favor reinserir.\n{separador}");
+        InserirNumeroDaConta();
+    }
+    else
+    {
+        System.Console.WriteLine(separador);
+        PerguntarDepositoInicial();
+    }    
+}
+//Inserir depósito inicial ou não, com estrutura de condição para o usuário decidir
+void PerguntarDepositoInicial()
+{
+    System.Console.WriteLine("Deseja realizar um depósito inicial?");
+    fazerdepositoinicial=Console.ReadLine();
+    if(fazerdepositoinicial.ToLower().Trim()=="sim")
+    {
+        InserirDepositoInicial();
+    }
+    else
+    {    
+        SemDepositoInicial();
+    }    
+}
+void InserirDepositoInicial()
+{
+    System.Console.WriteLine(separador);
     System.Console.WriteLine("Digite o valor do depósito inicial");
-    double depositoinicial=double.Parse(Console.ReadLine());
-    c1=new Cliente(nome,numerodaconta,depositoinicial);
+    string digitardepositoinicial=Console.ReadLine();
+    if(string.IsNullOrWhiteSpace(digitardepositoinicial)||digitardepositoinicial.All(char.IsLetter)||digitardepositoinicial=="0")
+    {
+        System.Console.WriteLine($"Campo em branco, com caracteres ou zerado, favor reinserir.");
+        InserirDepositoInicial();
+    }
+    else
+    {
+        double.TryParse(digitardepositoinicial,out double depositoinicial);        
+        c1=new Cliente(nome,numerodaconta,depositoinicial);       
+    }
 }
-else
+void SemDepositoInicial()
 {
-   c1=new Cliente(nome,numerodaconta);
+    if(fazerdepositoinicial.ToLower().Trim()=="não")
+    {
+        c1=new Cliente(nome,numerodaconta);                      
+    }    
+    else 
+    {
+        System.Console.WriteLine($"Inserção incorreta, favor reinserir.\n{separador}");
+        PerguntarDepositoInicial();
+    }
 }
-System.Console.WriteLine(separador);
-Console.ReadKey();
-Console.Clear();
-System.Console.WriteLine(separador);
-System.Console.WriteLine("Digite o valor do depósito");
-double Deposito=double.Parse(Console.ReadLine());
-c1.RealizarDeposito(Deposito);
-Console.ReadKey();
-Console.Clear();
-System.Console.WriteLine(separador);
-System.Console.WriteLine("Digite o valor do saque (com taxa de 5 reais)");
-double Saque=double.Parse(Console.ReadLine());
-c1.RealizarSaque(Saque);
-Console.ReadKey();
-Console.Clear();
+//Inserir depósito de dinheiro para o saldo, com estrutura de condição para validar o input
+void InserirDeposito()
+{
+    System.Console.WriteLine(separador);
+    System.Console.WriteLine("Digite o valor do depósito");
+    Deposito=Console.ReadLine();
+    if(string.IsNullOrWhiteSpace(Deposito)||Deposito.All(char.IsLetter)||Deposito=="0")
+    {
+        System.Console.WriteLine($"Campo em branco, com caracteres ou zerado, favor reinserir.");
+        InserirDeposito();
+    }
+    else
+    {
+        double.TryParse(Deposito,out double deposito);
+        c1.RealizarDeposito(deposito);
+    }
+}
+//Inserir valor de saque do saldo com taxa de 5 reais, com estrutura de condição para validar o input
+void InserirSaque()
+{
+    System.Console.WriteLine(separador);
+    System.Console.WriteLine("Digite o valor do saque (com taxa de 5 reais)");
+    Saque=Console.ReadLine();
+    if(string.IsNullOrWhiteSpace(Saque)||Saque.All(char.IsLetter)||Saque=="0")
+    {
+        System.Console.WriteLine($"Campo em branco, com caracteres ou zerado, favor reinserir.");
+        InserirSaque();
+    }
+    else
+    {
+        double.TryParse(Saque,out double saque);
+        c1.RealizarSaque(saque);
+    }
+}
